baf0a425ca9621ef00f294a35cb7e1fdaf499ae2
exposed <exposed@email.com>
2024-01-27 01:26:55 -0500

    no internal

--- docs.go
+++ docs.go
@@ -3,6 +3,6 @@
 package caddy
 
 //go:generate rm -rf docs
-//go:generate go run "github.com/johnstarich/go/gopages" -base /$GOPACKAGE -internal -out docs
+//go:generate go run "github.com/johnstarich/go/gopages" -base /$GOPACKAGE -out docs
 
 import _ "github.com/johnstarich/go/gopages"
