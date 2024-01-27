7587b80227272782e65ee36c309780e6fb761eff
exposed <exposed@email.com>
2024-01-27 01:10:16 -0500

    no base?

--- docs.go
+++ docs.go
@@ -3,6 +3,6 @@
 package caddy
 
 //go:generate rm -rf docs
-//go:generate go run "github.com/johnstarich/go/gopages" -base /$GOPACKAGE -internal -out docs -source-link "https://github.com/point-c/$GOPACKAGE/blob/main/{{.Path}}{{if .Line}}#L{{.Line}}{{end}}"
+//go:generate go run "github.com/johnstarich/go/gopages" -internal -out docs -source-link "https://github.com/point-c/$GOPACKAGE/blob/main/{{.Path}}{{if .Line}}#L{{.Line}}{{end}}"
 
 import _ "github.com/johnstarich/go/gopages"
